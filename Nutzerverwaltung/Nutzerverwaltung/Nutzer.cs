c7d57d5dbc6e43dcde724e2842168c7047f74206
ZobelChristoph <zobel@klara-oppenheimer-schule.de>
2020-04-19 20:48:34 +0200

    Nutzerverwaltung

---
+++ Nutzerverwaltung/Nutzerverwaltung/Nutzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutzerverwaltung
+{
+    class Nutzer
+    {
+        private string vorname;    
+        private string nachname;
+        
+        /* Definition des Konstruktors der Klasse Mitarbeiter */
+        public Nutzer(string vorname, string nachname)
+        {
+            this.vorname = vorname;
+            this.nachname = nachname;
+        }
+
+        public string getVorname()
+        {
+            return this.vorname;
+        }
+
+        public string getNachname()
+        {
+            return this.nachname;
+        }
+    }
+}
---
+++ Nutzerverwaltung/Nutzerverwaltung/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutzerverwaltung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vorname:");
+            string vorname = Console.ReadLine();
+            Console.WriteLine("Nachname:");
+            string nachname = Console.ReadLine();
+
+            Nutzer ersterNutzer = new Nutzer(vorname, nachname);
+            Console.WriteLine(ersterNutzer.getVorname());
+            Console.WriteLine(ersterNutzer.getNachname());
+
+            Console.ReadLine();
+        }
+    }
+}
