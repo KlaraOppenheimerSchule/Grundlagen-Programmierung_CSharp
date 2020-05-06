a31e33e6dffaf35946f6776279c88dc23ee5a980
s4nd3r-m4nd3r <schmittsandro40@gmail.com>
2020-05-06 09:12:10 +0200

    + caesarCipher

---
+++ 01_Einführung_OOP/CaesarCipher/SandroSchmitt/Aufgabe 2 - Verschlüsselung+Entschlüsselung/Cryptograph.cs
@@ -0,0 +1,70 @@
+﻿using NetSpell.SpellChecker;
+using NetSpell.SpellChecker.Dictionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Aufgabe_2___Verschlüsselung_Entschlüsselung
+{
+    class Cryptograph
+    {
+        public string Encrypt(string textToEncrypt, int key)
+        {
+            string encryptedString = "";
+
+            foreach (char character in textToEncrypt)
+            {
+                int asciiChar = Convert.ToInt32(character) + key;
+                asciiChar = asciiChar > 126 ? asciiChar - 95 : asciiChar;
+
+                encryptedString += (char)asciiChar;
+            }
+
+            return encryptedString;
+        }
+
+        public string Decrypt(string textToDecrypt, int key)
+        {
+            string decryptedString = "";
+
+            foreach (char character in textToDecrypt)
+            {
+                int asciiChar = Convert.ToInt32(character) - key;
+                asciiChar = asciiChar < 32 ? asciiChar + 95 : asciiChar;
+
+                decryptedString += (char)asciiChar;
+            }
+
+            return decryptedString;
+        }
+
+        public void FindWord(string text)
+        {
+            bool wordFound = false;
+            string foundText = "";
+
+            Spelling oSpell = new Spelling();
+
+            for (int key = 1; key <= 95; key++)
+            {
+                string textToTest = Decrypt(text, key);
+                string[] words = textToTest.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (oSpell.TestWord(word))
+                    {
+                        wordFound = true;
+                        foundText = textToTest;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(wordFound ? foundText : "Kein Wort gefunden!");
+        }
+    }
+}
---
+++ 01_Einführung_OOP/CaesarCipher/SandroSchmitt/Aufgabe 2 - Verschlüsselung+Entschlüsselung/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2___Verschlüsselung_Entschlüsselung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool showMenu = true;
+
+                while (showMenu)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Bitte eine Option auswählen:");
+                    Console.WriteLine("1) Verschlüsseln");
+                    Console.WriteLine("2) Entschlüsseln");
+                    Console.WriteLine("3) Wort herausfinden");
+                    Console.WriteLine("4) Exit");
+                    Console.Write("\r\nOption auswählen: ");
+
+
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.Clear();
+                            Console.WriteLine("Bitte Text zum Verschlüsseln eingeben:");
+                            string input = Console.ReadLine();
+
+                            Console.WriteLine("Bitte Schlüssel eingeben:");
+                            int key = Convert.ToInt32(Console.ReadLine());
+
+                            Cryptograph cryptograph = new Cryptograph();
+                            Console.WriteLine(cryptograph.Encrypt(input, key));
+                            Console.ReadLine();
+                            break;
+
+                        case "2":
+                            Console.Clear();
+                            Console.WriteLine("Bitte Text zum Entschlüsseln eingeben:");
+                            string input2 = Console.ReadLine();
+
+                            Console.WriteLine("Bitte Schlüssel eingeben:");
+                            int key2 = Convert.ToInt32(Console.ReadLine());
+
+                            Cryptograph cryptograph2 = new Cryptograph();
+                            Console.WriteLine(cryptograph2.Decrypt(input2, key2));
+                            Console.ReadLine();
+                            break;
+
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("Bitte Wort eingeben:");
+                            string input3 = Console.ReadLine();
+
+                            Cryptograph cryptograph3 = new Cryptograph();
+                            cryptograph3.FindWord(input3);
+                            Console.ReadLine();
+                            break;
+
+                        case "4":
+                            showMenu = false;
+                            break;
+                        default:
+                            showMenu = false;
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
