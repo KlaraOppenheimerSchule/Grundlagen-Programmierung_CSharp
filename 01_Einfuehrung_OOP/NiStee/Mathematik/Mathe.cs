cef6a864f7ce25b85124ec3cf86d2747fc5a1b7b
niStee <52573120+niStee@users.noreply.github.com>
2020-10-22 11:19:50 +0200

    Update Mathe.cs

--- 01_Einfuehrung_OOP/NiStee/Mathematik/Mathe.cs
+++ 01_Einfuehrung_OOP/NiStee/Mathematik/Mathe.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace Mathematik {
     public class Mathe {
