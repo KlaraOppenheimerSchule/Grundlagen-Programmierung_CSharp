8f4469540d320efac46b58a4727df3704cc04779
niStee <52573120+niStee@users.noreply.github.com>
2020-10-22 11:03:54 +0200

    Add files via upload

---
+++ 01_Einfuehrung_OOP/NiStee/Bibliothek/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Bibliothek {
+    public class Book {
+        // private string Isbn();
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+        public Book(string isbn, string title, string author, double price) {
+            this.Isbn = isbn;
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+        }
+        /* public string getIsbn{
+         * return this.Isbn;
+         * }
+         * 
+         * public string setIsbn(string isbn){
+         * this.Isbn = isbn;
+         * }
+         */
+    }
+}
