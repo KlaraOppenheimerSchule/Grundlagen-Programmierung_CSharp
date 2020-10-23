75140349de435f8716aeb74d78fb028c1752d816
niStee <52573120+niStee@users.noreply.github.com>
2020-10-23 15:23:49 +0200

    Update Program.cs

--- 01_Einfuehrung_OOP/NiStee/Mathematik/Program.cs
+++ 01_Einfuehrung_OOP/NiStee/Mathematik/Program.cs
@@ -3,21 +3,8 @@ using System;
 namespace Mathematik {
     class Program {
         static void Main(string[] args) {
-            var neueRechnung = new Mathe();    
-            neueRechnung.Summe(5, 4);
-            Console.WriteLine(neueRechnung.Ergebnis);
-
-            var neueRechnung2 = new Mathe();
-            neueRechnung2.Fakul(20);
-            Console.WriteLine(neueRechnung2.Ergebnis);
-
-            var neueRechnung3 = new Mathe();
-            neueRechnung3.Quadrat(6);
-            Console.WriteLine(neueRechnung3.Ergebnis);
-
-            var neueRechnung4 = new Mathe();
-            neueRechnung4.Quadrat(-6);
-            Console.WriteLine(neueRechnung3.Ergebnis);
-        }
+            Console.WriteLine(Mathe.Summe(2, 4));
+            Console.WriteLine(Mathe.Quadrat(4));
+            Console.WriteLine(Mathe.Fakul(-2));
     }
 }
