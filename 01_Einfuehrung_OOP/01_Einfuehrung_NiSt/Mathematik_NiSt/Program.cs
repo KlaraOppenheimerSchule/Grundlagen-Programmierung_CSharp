55bea24171df8d1b2b6ed849e791b10985a25efc
niStee <52573120+niStee@users.noreply.github.com>
2021-02-04 12:52:28 +0100

    Umbenennung der Ordner und der Solution Datein, nach Vorgabe dieses Projekts.

---
+++ 01_Einfuehrung_OOP/01_Einfuehrung_NiSt/Mathematik_NiSt/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Mathematik {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Mathe.Summe(2, 4));
+            Console.WriteLine(Mathe.Quadrat(4));
+            Console.WriteLine(Mathe.Fakul(-2));
+    }
+}
---
+++ 01_Einfuehrung_OOP/01_Einfuehrung_NiSt/Nutzerverwaltung_NiSt/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nutzerverwaltung {
+    class Program {
+        static void Main(string[] args) {
+            Mitarbeiter neuerMitarbeiter = new Mitarbeiter("Steenebrügge", "Nils");
+            Console.WriteLine($"Ihr neuer Mitarbeiter {neuerMitarbeiter.Vorname} {neuerMitarbeiter.Nachname} wurde im System angelegt.");
+            Console.ReadKey();
+        }
+    }
+}
