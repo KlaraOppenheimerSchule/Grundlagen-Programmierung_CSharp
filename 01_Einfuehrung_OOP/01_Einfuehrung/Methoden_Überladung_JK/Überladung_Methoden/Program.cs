b99ee2565c3e807b5fe43d8ac81eaadf726b0629
Joschi95 <joschka.nehls@gmail.com>
2021-10-19 09:36:49 +0200

    Überladung_Methoden_JK

---
+++ 01_Einfuehrung_OOP/01_Einfuehrung/Methoden_Überladung_JK/Überladung_Methoden/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Überladung_Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple Granny_Smith = new Apple("green", 2.56);
+            Granny_Smith.SetProperty("red");
+            Granny_Smith.SetProperty(1.456);
+            Console.WriteLine(Granny_Smith.GetColour());
+            Console.WriteLine(Granny_Smith.GetWheight());
+        }
+        class Apple
+        {
+            private string Colour;
+            private double wheight;
+
+            public Apple(string Colour, double wheight)
+            {
+                this.Colour = Colour;
+                this.wheight = wheight;
+            }
+
+            public void SetProperty(string Colour)
+            {
+                this.Colour = Colour;
+            }
+            public void SetProperty(double wheight)
+            {
+                this.wheight = wheight;
+            }
+            public string GetColour()
+            {
+                return this.Colour;
+            }
+
+            public double GetWheight()
+            {
+                return this.wheight;
+            }
+        }
+    }
+}
